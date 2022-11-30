254da5cde354d09bb3da012166c2df4f1618051e
Daniel Shin <kyuds1016@berkeley.edu>
2022-11-29 18:05:52 -0800

    finish

--- generate.py
+++ generate.py
@@ -5,12 +5,37 @@ import random
 import argparse
 from datetime import datetime
 
-def overlay(data):
-    pass
+def overlay(img, ovrly):
+    row, col, _ = img.shape
+    
+    for r in range(row):
+        for c in range(col):
+            a = float(ovrly[r][c][3] / 255.0)
+            img[r][c] = a * ovrly[r][c] + (1 - a) * img[r][c]
+
+    return img
+
+def create(data):
+    img = None
+
+    for layer, item in data:
+        path = "assets/" + str(layer) + "/" + item + ".png"
+
+        if layer == 0:
+            img = cv2.imread(path, cv2.IMREAD_UNCHANGED)
+            continue
+
+        if item == "None":
+            continue
+        
+        ovrly = cv2.imread(path, cv2.IMREAD_UNCHANGED)
+        img = overlay(img, ovrly)
+
+    return img
 
 def main():
     # parse arguments
-    parser = argparse.ArgumentParser(description='Options for NFT generation.')
+    parser = argparse.ArgumentParser(description='options for NFT generation.')
     parser.add_argument('--n', default=1, type=int, help='number of NFTs to generate.')
     args = parser.parse_args()
 
@@ -25,13 +50,13 @@ def main():
         for layer in data.keys():
             w = [int(data[layer][x]) for x in data[layer].keys()]
             v = list(data[layer].keys())
-            config.append((layer, random.choices(v, weights=w, k=1)))
-        
+            config.append((int(layer), random.choices(v, weights=w, k=1)[0]))
+        config.sort(key=lambda x: x[0])
+
         # write image result
-        nft = overlay(config)
-        path = "results/" + str(datetime.now()) + ".png"
-        print(path)
-        cv2.imwrite(nft, path)
+        nft = create(config)
+        path = "result/" + str(datetime.now()) + ".png"
+        cv2.imwrite(path, nft)
 
 if __name__ == "__main__":
     main()
