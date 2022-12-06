cfe497b06ef558f12eaca4d5da0a0d21892c98e6
Daniel Shin <88547237+kyuds@users.noreply.github.com>
2022-12-05 16:50:24 -0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,11 +1,14 @@
 number="$1"
+fail="$2"
 
 if [ ! -d "result" ]; then
     mkdir result
 fi
 
-if [ ! $number ]; then
+if [[ ! $number ]] && [[ ! $fail ]]; then
     python generate.py
-else
+elif [[ ! $fail ]]; then
     python generate.py --n $number
+elif [[ ! $number ]]; then
+    python generate.py --f $fail
 fi
