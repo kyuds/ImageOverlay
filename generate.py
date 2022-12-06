a077bcdae19d5d8e8b751eb04f676c4719c72ef0
Daniel Shin <kyuds1016@berkeley.edu>
2022-12-05 16:46:41 -0800

    add csv history tracking

--- generate.py
+++ generate.py
@@ -3,15 +3,17 @@ import json
 import random
 
 import argparse
+import csv
+from pathlib import Path
 from datetime import datetime
 
 def overlay(img, ovrly):
     row, col, _ = img.shape
+
     for r in range(row):
         for c in range(col):
             if ovrly[r][c][3] == 0:
                 continue
-            
             a = float(ovrly[r][c][3] / 255.0)
             img[r][c] = a * ovrly[r][c] + (1 - a) * img[r][c]
 
@@ -19,6 +21,7 @@ def overlay(img, ovrly):
 
 def create(data):
     img = None
+
     for layer, item in data:
         path = "assets/" + str(layer) + "/" + item + ".png"
 
@@ -37,6 +40,7 @@ def main():
     # parse arguments
     parser = argparse.ArgumentParser(description='options for NFT generation.')
     parser.add_argument('--n', default=1, type=int, help='number of NFTs to generate.')
+    parser.add_argument('--f', default=100, type=int, help='number of duplicates to tolerate.')
     args = parser.parse_args()
 
     # load in configurations
@@ -44,26 +48,41 @@ def main():
     with open('config.json') as file:
         data = json.load(file)
 
+    # load (or create) csv for past NFTs
+    file = Path("gen.csv")
+    file.touch(exist_ok=True)
+
     generated = set()
+    with open("gen.csv", "r") as csvfile:
+        csvreader = csv.reader(csvfile)
+        for line in csvreader:
+            generated.add(tuple(line))
 
-    for _ in range(args.n):
-        # random generation
-        config = []
-        for layer in data.keys():
-            w = [int(data[layer][x]) for x in data[layer].keys()]
-            v = list(data[layer].keys())
-            config.append((int(layer), random.choices(v, weights=w, k=1)[0]))
-        config.sort(key=lambda x: x[0])
+    # generate NFTs
+    with open("gen.csv", "a") as csvfile:
+        _, f = 0, 0
+        csvwriter = csv.writer(csvfile)
+        while _ < args.n and f < args.f:
+            # random generation
+            config = []
+            for layer in data.keys():
+                w = [int(data[layer][x]) for x in data[layer].keys()]
+                v = list(data[layer].keys())
+                config.append((int(layer), random.choices(v, weights=w, k=1)[0]))
+            config.sort(key=lambda x: x[0])
 
-        hashable = tuple(config)
-        if hashable in generated:
-            continue
-        generated.add(hashable)
+            hashable = tuple([x[1] for x in config])
+            if hashable in generated:
+                f += 1
+                continue
+            generated.add(hashable)
+            csvwriter.writerow(hashable)
 
-        # write image result
-        nft = create(config)
-        path = "result/" + str(datetime.now()) + ".png"
-        cv2.imwrite(path, nft)
+            # write image result
+            nft = create(config)
+            path = "result/" + str(datetime.now()) + ".png"
+            cv2.imwrite(path, nft)
+            _ += 1
 
 if __name__ == "__main__":
     main()
